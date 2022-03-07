5d1b03fd980c4110c6c26eed2f51c7d2b68cd80d
priyanka-bhansali <priyanka.bhansali@aleks.com>
2022-03-07 06:48:08 -0800

    Modified iterative binary tree search with some additional info

--- Trees/BinaryTreeSearch.java
+++ Trees/BinaryTreeSearch.java
@@ -9,7 +9,7 @@ class Node {
 }
 
 class BinaryTree {
-  void search(Node root, int x) {
+  void searchBST(Node root, int x) {
     while (root != null) {
       if (root.data == x) {
         System.out.println("Search Found");
@@ -23,6 +23,31 @@ class BinaryTree {
     }
     System.out.println("Not Found");
   }
+
+  void search(Node root, int x) {
+    Node parent = null;
+    while (root != null) {
+      if (root.data == x) {
+        if (parent == null) {
+          System.out.println("The node with key " + x + " is root node.");
+          return;
+        }
+        if (x < parent.data) { // this is parent.data and not root.data -> root data and data are equal so needs to be compared to the parent
+          System.out.println("Key is at LEFT node of tree with parent node = " + parent.data);
+        } else {
+          System.out.println("Key is at RIGHT node of tree with parent node = " + parent.data);
+        }
+        return;
+      }
+      parent = root;
+      if (x < root.data) {
+        root = root.left;
+      } else {
+        root = root.right;
+      }
+    }
+    System.out.println("Key not found.");
+  }
 }
 
 class Solution {
@@ -33,7 +58,13 @@ class Solution {
     root.right.right = new Node(12);
 
     BinaryTree tree = new BinaryTree();
-    tree.search(root, 5);
+    tree.searchBST(root, 5);
+    tree.searchBST(root, 9);
+
+    tree.search(root, 4);
+    tree.search(root, 12);
+    tree.search(root, 7);
+    tree.search(root, 3);
     tree.search(root, 9);
   }
 }
